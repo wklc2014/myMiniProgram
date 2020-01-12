3db5eea66e0a4b659836edc85f66826dac723156
wangkun <18111224812@163.com>
2020-01-12 20:40:09 +0800

    wechat

---
+++ pages/news/news.js
@@ -0,0 +1,21 @@
+Page({
+  data: {
+    hotList: [
+      {
+        id: 1,
+        name: '成都',
+      },
+      {
+        id: 2,
+        name: '绵阳',
+      },
+      {
+        id: 3,
+        name: '重庆',
+      },
+    ]
+  },
+  onLoad: function () {
+    console.log('news page is load');
+  }
+})
